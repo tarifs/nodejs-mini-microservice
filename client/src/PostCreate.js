2472a284c20e5f6241e5f9b3f0f574cd5a070a82
Tarif Hossain <tarif@namespaceit.com>
2022-07-31 01:09:41 +0600

    implement react app with posts service

---
+++ client/src/PostCreate.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (!title) return;
+
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle("");
+    window.location.reload(false);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
