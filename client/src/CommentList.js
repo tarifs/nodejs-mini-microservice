ec7335104bde2b48870ef8e95b655e04af874940
Tarif Hossain <tarif@namespaceit.com>
2022-08-07 00:32:14 +0600

    add moderation service with some code modification

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,23 +1,22 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (content.status === "approved") {
+      content = comment.content;
+    }
+
+    if (content.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (content.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
--- event-bus/index.js
+++ event-bus/index.js
@@ -4,16 +4,23 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+  events.push(event);
+  axios.post("http://localhost:4000/events", event); //posts service
+  axios.post("http://localhost:4001/events", event); //comments service
+  axios.post("http://localhost:4002/events", event); //query service
+  axios.post("http://localhost:4003/events", event); //moderation service
 
   res.send({ status: "OK" });
 });
 
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 
 app.listen(4005, () => {
-    console.log('Listening port on 4005')
-})
+  console.log("Listening port on 4005");
+});
