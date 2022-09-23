948868d58acd1b415910856dc15592bac3655cb1
Tarif Hossain <tarif@namespaceit.com>
2022-09-24 00:59:45 +0600

    kubernetes implement

--- posts/index.js
+++ posts/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
-const { default: axios } = require("axios");
+const axios = require("axios");
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
